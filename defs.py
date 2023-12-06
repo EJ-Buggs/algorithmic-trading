253bd260bc2791f3ccb7d5682e783e315a3b7aa3
Emmanuel <your.email@example.com>
2023-12-06 08:53:26 -0500

    add api, account#, vevn, jupyter, etc install packages.

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "2320f8371ec3a1938a725de94324cba7-c612928ae04561642a7ca6e196daadc0"
+ACCOUNT_ID = "101-001-27489114-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
